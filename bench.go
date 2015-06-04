ea1ab7fa992f053a283025e87a7799adb1bcba5e
Makoto Sasaki <wanigoya@gmail.com>
2015-06-04 15:30:27 +0900

    Extract Config

--- bench.go
+++ bench.go
@@ -1,74 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/codegangsta/cli"
-	"io/ioutil"
 	"os"
-	"strings"
 )
-
-type Config struct {
-	URLs          []string `json:"url"`
-	MaxAccess     int      `json:"count"`
-	MaxWorkers    int      `json:"worker"`
-	BasicAuthUser string   `json:"basic-auth-user"`
-	BasicAuthPass string   `json:"basic-auth-pass"`
-}
-
-func (c *Config) Validate() error {
-	if len(c.URLs) == 0 {
-		return errors.New("urlは必須です")
-	}
-	if c.MaxAccess == 0 {
-		return errors.New("countは必須です")
-	}
-	if c.MaxWorkers == 0 {
-		return errors.New("workerは必須です")
-	}
-	return nil
-}
 
 func bench(c *cli.Context) {
 
 	var config Config
 
-	configFile := c.String("config-file")
-	if configFile != "" {
-		configData, err := ioutil.ReadFile(configFile)
-		if err != nil {
-			fmt.Println(err)
-			cli.ShowAppHelp(c)
-			os.Exit(1)
-		}
-
-		err = json.Unmarshal(configData, &config)
-		if err != nil {
-			fmt.Println(err)
-			cli.ShowAppHelp(c)
-			os.Exit(1)
-		}
-	}
-
-	if c.String("url") != "" {
-		config.URLs = strings.Split(c.String("url"), ",")
-	}
-	if c.Int("count") != 0 {
-		config.MaxAccess = c.Int("count")
-	}
-	if c.Int("worker") != 0 {
-		config.MaxWorkers = c.Int("worker")
-	}
-	if c.String("basic-auth-user") != "" {
-		config.BasicAuthUser = c.String("basic-auth-user")
-	}
-	if c.String("basic-auth-pass") != "" {
-		config.BasicAuthPass = c.String("basic-auth-pass")
-	}
-
-	if err := config.Validate(); err != nil {
+	if err := config.Create(c); err != nil {
 		fmt.Println(err)
 		cli.ShowAppHelp(c)
 		os.Exit(1)
---
+++ config.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"strings"
+)
+
+type Config struct {
+	URLs          []string `json:"url"`
+	MaxAccess     int      `json:"count"`
+	MaxWorkers    int      `json:"worker"`
+	BasicAuthUser string   `json:"basic-auth-user"`
+	BasicAuthPass string   `json:"basic-auth-pass"`
+}
+
+func (config *Config) Create(context *cli.Context) error {
+
+	configFile := context.String("config-file")
+	if configFile != "" {
+		if err := config.setFromConfigFile(configFile); err != nil {
+			return err
+		}
+	}
+
+	config.setFromArgs(context)
+
+	if err := config.validate(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Config) setFromConfigFile(configFile string) error {
+	configData, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(configData, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (config *Config) setFromArgs(context *cli.Context) {
+	if context.String("url") != "" {
+		config.URLs = strings.Split(context.String("url"), ",")
+	}
+	if context.Int("count") != 0 {
+		config.MaxAccess = context.Int("count")
+	}
+	if context.Int("worker") != 0 {
+		config.MaxWorkers = context.Int("worker")
+	}
+	if context.String("basic-auth-user") != "" {
+		config.BasicAuthUser = context.String("basic-auth-user")
+	}
+	if context.String("basic-auth-pass") != "" {
+		config.BasicAuthPass = context.String("basic-auth-pass")
+	}
+}
+
+func (c *Config) validate() error {
+	if len(c.URLs) == 0 {
+		return errors.New("urlは必須です")
+	}
+	if c.MaxAccess == 0 {
+		return errors.New("countは必須です")
+	}
+	if c.MaxWorkers == 0 {
+		return errors.New("workerは必須です")
+	}
+	return nil
+}
